7648abbdfdced822de78b50190487cf39f6ad184
Samuel Ribeiro <samuelribeiro9@icloud.com>
2025-08-29 15:59:14 -0300

    criando teste do use case create client

---
+++ internal/usecase/create_client/create_client_test.go
@@ -0,0 +1,42 @@
+package create_client
+
+import (
+	"testing"
+
+	"github.com/SamuelDevMobile/ms-wallet/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type ClientGatewayMock struct {
+	mock.Mock
+}
+
+func (m *ClientGatewayMock) Get(id string) (*entity.Client, error) {
+	args := m.Called(id)
+	return args.Get(0).(*entity.Client), args.Error(1)
+}
+
+func (m *ClientGatewayMock) Save(client *entity.Client) error {
+	args := m.Called(client)
+	return args.Error(0)
+}
+
+func TestCreateClientUseCase_Execute(t *testing.T) {
+	m := &ClientGatewayMock{}
+	m.On("Save", mock.Anything).Return(nil)
+	uc := NewCreateClientUseCase(m)
+
+	output, err := uc.Execute(CreateClientInputDTO{
+		Name:  "Samuel",
+		Email: "s@s.com",
+	})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	assert.NotEmpty(t, output.ID)
+	assert.Equal(t, "Samuel", output.Name)
+	assert.Equal(t, "s@s.com", output.Email)
+	m.AssertExpectations(t)
+	m.AssertNumberOfCalls(t, "Save", 1)
+}
