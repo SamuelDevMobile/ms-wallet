be4cf04fd848a5378c9f3e752e762fc450301f22
Samuel Ribeiro <samuelribeiro9@icloud.com>
2025-09-16 00:35:02 -0300

    enviando enventos para o kafka um balance

--- internal/usecase/create_transaction/create_transaction_test.go
+++ internal/usecase/create_transaction/create_transaction_test.go
@@ -54,10 +54,11 @@ func TestCreateTransactionUseCase_Execute(t *testing.T) {
 	}
 
 	dispatcher := events.NewEventDispatcher()
-	eventTest := event.NewTransactionCreated()
+	eventTransaction := event.NewTransactionCreated()
+	eventBalance := event.NewBalanceUpdated()
 	ctx := context.Background()
 
-	uc := NewCreateTransactionUseCase(mockUow, dispatcher, eventTest)
+	uc := NewCreateTransactionUseCase(mockUow, dispatcher, eventTransaction, eventBalance)
 	output, err := uc.Execute(ctx, inputDto)
 	assert.Nil(t, err)
 	assert.NotNil(t, output)
